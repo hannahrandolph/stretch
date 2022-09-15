0caf150a011bbe2422621f946b19ecc42a8b66f6
Hannah Randolph <hannah.randolph@covermymeds.com>
2022-09-15 13:24:55 -0400

    progress

--- Gigapet/lib/game.rb
+++ Gigapet/lib/game.rb
@@ -1,3 +1,22 @@
 class Game
+    attr_writer :start
+    attr_reader :welcome
+
+    def initialize(start, welcome)
+        @@start = start
+        @@welcome = welcome
+    end 
     
+    def welcome_message
+        puts "Welcome to Gigapet!"
+
+        
+    end
+
+    def start
+    end
+
+    def choose_action
+
+    end
 end
